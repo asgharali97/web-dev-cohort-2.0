90cf7229a9027bc33469c5cc23902772202ab5ff
Asghar Ali <asgharalifs48@gmail.com>
2026-02-22 20:17:31 +0500

    write polyfills for reduce, flat, and forEach methods

--- assignments/pollyfills/pollyfills-array.js
+++ assignments/pollyfills/pollyfills-array.js
@@ -60,3 +60,74 @@ Array.prototype.myFilter = function (cb) {
 let mixNums = [1, 2, 3, 4, 5]
 let filtery = mixNums.myFilter((item) => item % 2 === 0)
 console.log(filtery);
+
+
+// ++++ reduce pollyfill +++++
+// raw reduce 
+
+function reducy(prev, curr) {
+    return prev += curr
+}
+let acc = 0;
+for (let i = 0; i < mixNums.length; i++) {
+    acc = reducy(acc, mixNums[i]);
+}
+
+// my reduce pollyfill 
+Array.prototype.myReduce = function (cb, init) {
+    let acc = init;
+
+    for (let i = 0; i < this.length; i++) {
+        acc = cb(acc, this[i]);
+    }
+    return acc
+}
+
+let sum = mixNums.myReduce(function (prev, curr) {
+    return prev += curr
+}, 0)
+
+console.log(sum)
+
+// flat poyllyfill
+// flat raw way
+
+let f = [1, 2, 3, [4, 5, [6, 7]]]
+function myFlaty(arr, depth) {
+    let flatyResult = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i]) && depth > 0) {
+            let flated = myFlaty(arr[i], depth - 1)
+            flatyResult.push(...flated)
+        } else flatyResult.push(arr[i])
+    }
+    return flatyResult
+}
+
+console.log(myFlaty(f, 2));
+
+// my Flat pollyfill
+Array.prototype.myFlat = function (depth) {
+    function myFlaty(arr, depth) {
+        let flatyResult = [];
+
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i]) && depth > 0) {
+                let flated = myFlaty(arr[i], depth - 1)
+                flatyResult.push(...flated)
+            } else flatyResult.push(arr[i])
+        }
+        return flatyResult
+    }
+    return myFlaty(this, depth)
+}
+
+// for eact pollyfill
+Array.prototype.myForEach = function (cb) {
+    for (let i = 0; i < this.length; i++) {
+        cb(this[i])
+    }
+}
+
+let e = arr.myForEach((item) => console.log(item));
